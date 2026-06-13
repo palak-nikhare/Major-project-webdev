fc92c549542d8f304ae17a72feadb9c9b8adfe43
Palak Nikhare <palaknikhare15@gmail.com>
2026-06-14 00:49:58 +0530

    Completed CRUD operations for listings

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 const listing = require("./models/listing.js")
 const path = require("path");
+const methodOverride = require("method-override");
 
 const mongo_url = "mongodb://127.0.0.1:27017/travelia";
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
 
 main().then((res)=>{
     console.log("connected to db");
@@ -43,7 +45,7 @@ app.get("/listings" ,async (req, res)=>{
     const alllistings =await listing.find({});
     res.render("listings/index.ejs", {alllistings})
     await listing.find({}).then((res)=> {
-        console.log(res);
+        console.log("");
     })
 })
 
@@ -53,9 +55,11 @@ app.get("/listings/new", (req, res ) =>{
 })
 
 //create route
-app.post("/listings" , (req ,res)=> {
-    let listing = req.body;
-    console.log(listing);
+app.post("/listings" , async(req ,res)=> {
+    const newlis = new listing(req.body.listing)
+    //here listing is mongoose.model
+    await newlis.save();
+    res.redirect("/listings");
 })
 
 //show route
@@ -65,6 +69,27 @@ app.get("/listings/:id" ,async (req , res)=>{
     res.render("listings/show.ejs", {Listing})
 })
 
+//edit route
+app.get("/listings/:id/edit", async (req,res)=>{
+    const {id}= req.params;
+    const list = await listing.findById(id);
+    res.render("listings/edit.ejs", {list});
+})
+
+//Update route
+app.put("/listings/:id" ,async (req, res)=> {
+    const {id}= req.params;
+    await listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+} )
+
+//delete route
+app.delete("/listings/:id",async (req,res)=> {
+    const {id}= req.params;
+    let dellist= await listing.findByIdAndDelete(id);
+    console.log(dellist);
+    res.redirect("/listings");
+})
 
 app.listen(8080, ()=> {
     console.log("server listening to port 8080");
