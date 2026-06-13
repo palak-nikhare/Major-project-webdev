0d218f020ac10e85064458330a6345b647a2566c
Palak Nikhare <palaknikhare15@gmail.com>
2026-06-13 11:37:41 +0530

    2nd commit

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongo_url = "mongodb://127.0.0.1:27017/travelia";
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended : true}));
 
 main().then((res)=>{
     console.log("connected to db");
@@ -45,6 +46,25 @@ app.get("/listings" ,async (req, res)=>{
         console.log(res);
     })
 })
+
+//new route
+app.get("/listings/new", (req, res ) =>{
+    res.render("listings/new.ejs");
+})
+
+//create route
+app.post("/listings" , (req ,res)=> {
+    let listing = req.body;
+    console.log(listing);
+})
+
+//show route
+app.get("/listings/:id" ,async (req , res)=>{
+    let {id} = req.params;
+    const Listing =await listing.findById(id);
+    res.render("listings/show.ejs", {Listing})
+})
+
 
 app.listen(8080, ()=> {
     console.log("server listening to port 8080");
